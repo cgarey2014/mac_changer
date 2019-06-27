cf1c427a90a4417bed8c98da6ca1ee8c1107b139
cgarey2014 <chrisgarey2014@gmail.com>
2019-06-27 16:06:12 -0400

    added security to subprocess.call

--- mac_changer.py
+++ mac_changer.py
@@ -7,6 +7,10 @@ new_mac = input("Choose new MAC address : ")
 
 print("[+] Changing MAC address for " + interface + "  to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
