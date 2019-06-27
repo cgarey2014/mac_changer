729f6dd6683c6eabbec6f5c43216f7c0397e9c44
cgarey2014 <chrisgarey2014@gmail.com>
2019-06-27 16:36:47 -0400

    added options and help display

--- mac_changer.py
+++ mac_changer.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python
 
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest=" interface", help="Interface you want to change the MAC address of")
+parser.add_option("-m", "--mac", dest=" new_mac", help=" The new MAC address")
+
+(options, arguments) = parser.parse_args()
 
 interface = input("Choose interface : ")
 new_mac = input("Choose new MAC address : ")
 
 print("[+] Changing MAC address for " + interface + "  to " + new_mac)
-
-# subprocess.call("ifconfig " + interface + " down", shell=True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call("ifconfig " + interface + " up", shell=True)
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
